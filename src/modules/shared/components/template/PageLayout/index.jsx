8162f60765cf2764fbb1b9e5e3ff9a0fff0b07ca
mohamedssabry <mo7amedssabry@gmailcom>
2025-03-21 15:37:43 +0200

    Refactored Navbar: Added NavLink for routing and improved active state styling.

---
+++ src/modules/shared/components/template/PageLayout/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../../../../Navbar";
+import { Outlet } from "react-router-dom";
+
+const PageLayout = () => {
+  return (
+    <div className="">
+      <Navbar />
+      <main className="">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default PageLayout;
